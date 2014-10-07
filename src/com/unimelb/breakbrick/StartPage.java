c4c1ffb59d541dbfc7c8937bbcbe81d204ca1231
zhankm <zhanibekkm@gmail.com>
2014-10-07 18:51:33 +1100

    promt dialogues

--- src/com/unimelb/breakbrick/StartPage.java
+++ src/com/unimelb/breakbrick/StartPage.java
@@ -1,12 +1,23 @@
 package com.unimelb.breakbrick;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
+import android.widget.EditText;
+import android.view.View.OnClickListener;
 
 public class StartPage extends Activity{
+	
+	final Context context = this;
+	private EditText result;
 
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -21,6 +32,64 @@ public class StartPage extends Activity{
                }
         });
         
+        
+        Button resumeGame = (Button) findViewById(R.id.button2);
+        resumeGame.setOnClickListener(new OnClickListener() {
+ 
+			@Override
+			public void onClick(View arg0) {
+ 
+				// get prompts.xml view
+				LayoutInflater li = LayoutInflater.from(context);
+				View promptsView = li.inflate(R.layout.prompts, null);
+ 
+				AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(
+						context);
+ 
+				// set prompts.xml to alertdialog builder
+				alertDialogBuilder.setView(promptsView);
+ 
+				final EditText userInput = (EditText) promptsView
+						.findViewById(R.id.editTextDialogUserInput);
+ 
+				// set dialog message
+				alertDialogBuilder
+					.setCancelable(false)
+					.setPositiveButton("OK",
+					  new DialogInterface.OnClickListener() {
+					    public void onClick(DialogInterface dialog,int id) {
+						// get user input and set it to result
+						// edit text
+						result.setText(userInput.getText());
+					    }
+					  })
+					.setNegativeButton("Cancel",
+					  new DialogInterface.OnClickListener() {
+					    public void onClick(DialogInterface dialog,int id) {
+						dialog.cancel();
+					    }
+					  });
+ 
+				// create alert dialog
+				AlertDialog alertDialog = alertDialogBuilder.create();
+ 
+				// show it
+				alertDialog.show();
+ 
+			}
+		});
+        
+        
+        Button highScore = (Button) findViewById(R.id.button3);
+        highScore.setOnClickListener(new View.OnClickListener() {
+        	 public void onClick(View view) {
+                 Intent myIntent = new Intent(getApplicationContext(), HighScore.class);
+                 startActivity(myIntent);
+                 finish();
+                }
+        });
+        
+      
         Button exit = (Button) findViewById(R.id.button5);
         exit.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
